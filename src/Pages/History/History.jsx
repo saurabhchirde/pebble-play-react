fcc72951bcd72eee3e9b5d75e89663be6977e3a5
saurabhchirde <saurabh123.chirde@gmail.com>
2022-04-14 15:51:02 +0530

    add: not logged message

--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -1,6 +1,7 @@
 import { VerticalCard } from "../../Components/Cards/VerticalCard";
 import Button from "../../Components/UI/Button/Button";
 import { useAlert, useAuth, useAxiosCalls, useVideo } from "../../Context";
+import { NotLogged } from "../../Components/NotLogged/NotLogged";
 import "./History.css";
 
 export const History = () => {
@@ -38,24 +39,30 @@ export const History = () => {
 
   return (
     <div className="history-body">
-      <div className="flex-row-center flex-justify-space-between">
-        <h1 className="history-title">
-          Watched History <i className="fas fa-history mg-point6-lt"></i>
-        </h1>
-        <Button
-          onClick={onRemoveAllHandler}
-          btnClassName="btn primary-outline-btn-md"
-          label="Remove all"
-        />
-      </div>
-      {history.length > 0 ? (
-        <div className="history-video-section flex-row">
-          {history.map((video) => (
-            <VerticalCard key={video._id} videoDetail={video} />
-          ))}
-        </div>
+      {token ? (
+        <>
+          <div className="flex-row-center flex-justify-space-between">
+            <h1 className="history-title">
+              Watched History <i className="fas fa-history mg-point6-lt"></i>
+            </h1>
+            <Button
+              onClick={onRemoveAllHandler}
+              btnClassName="btn primary-outline-btn-md"
+              label="Remove all"
+            />
+          </div>
+          {history.length > 0 ? (
+            <div className="history-video-section flex-row">
+              {history.map((video) => (
+                <VerticalCard key={video._id} videoDetail={video} />
+              ))}
+            </div>
+          ) : (
+            <h2 className="history-sub-title">No History Available </h2>
+          )}{" "}
+        </>
       ) : (
-        <h2 className="history-sub-title">No History Available </h2>
+        <NotLogged />
       )}
     </div>
   );
--- src/Pages/Watchlater/Watchlater.jsx
+++ src/Pages/Watchlater/Watchlater.jsx
@@ -1,25 +1,35 @@
 import { VerticalCard } from "../../Components/Cards/VerticalCard";
-import { useVideo } from "../../Context";
+import { useAuth, useModal, useVideo } from "../../Context";
+import { NotLogged } from "../../Components/NotLogged/NotLogged";
 import "./Watchlater.css";
 
 export const Watchlater = () => {
   const {
     videoState: { watchlater },
   } = useVideo();
+  const {
+    auth: { token },
+  } = useAuth();
 
   return (
     <div className="watchlater-body">
-      <h1 className="watchlater-title">
-        Watch Later <i className="fas fa-clock mg-point6-lt"></i>
-      </h1>
-      {watchlater.length > 0 ? (
-        <div className="watchlater-video-section flex-row">
-          {watchlater.map((video) => (
-            <VerticalCard key={video._id} videoDetail={video} />
-          ))}
-        </div>
+      {token ? (
+        <>
+          <h1 className="watchlater-title">
+            Watch Later <i className="fas fa-clock mg-point6-lt"></i>
+          </h1>
+          {watchlater.length > 0 ? (
+            <div className="watchlater-video-section flex-row">
+              {watchlater.map((video) => (
+                <VerticalCard key={video._id} videoDetail={video} />
+              ))}
+            </div>
+          ) : (
+            <h2 className="watchlater-sub-title">No videos in watchlater</h2>
+          )}
+        </>
       ) : (
-        <h2 className="watchlater-sub-title">No videos in watchlater</h2>
+        <NotLogged />
       )}
     </div>
   );
