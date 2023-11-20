9c3aaea3d653d84debfd5878db69306c1d09f98c
saurabhchirde <saurabh123.chirde@gmail.com>
2023-11-20 23:54:44 +0530

    fix: first render

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -24,27 +24,30 @@ export const Landing = () => {
 
   return (
     <div className="landing-body">
-      {videos.length > 0 && (
-        <>
-          <div className="landing-header-section">
-            <div className="landing-banner-section">
-              <BannerCard />
-            </div>
-            <div className="landing-banner-text">
-              <h1>Curated content - by Photographers, for Photographers</h1>
-              <p>
-                A video library for all photography enthusiast, to share,
-                shocase their work and learn new skill from other creators
-              </p>
-            </div>
-            <div className="landing-category-section">{mapCategories}</div>
+      <>
+        <div className="landing-header-section">
+          <div className="landing-banner-section">
+            <BannerCard />
           </div>
-          <div className="landing-must-watch-container">
-            <h1 className="video-title">Popular Videos</h1>
-            <div className="landing-must-watch-videos">{mapVideos}</div>
+          <div className="landing-banner-text">
+            <h1>Curated content - by Photographers, for Photographers</h1>
+            <p>
+              A video library for all photography enthusiast, to share, shocase
+              their work and learn new skill from other creators
+            </p>
           </div>
-        </>
-      )}
+          <div className="landing-category-section">
+            {categories.length > 0 && mapCategories}
+          </div>
+        </div>
+        <div className="landing-must-watch-container">
+          <h1 className="video-title">Popular Videos</h1>
+          <div className="landing-must-watch-videos">
+            {videos?.length > 0 && mapVideos}
+          </div>
+        </div>
+      </>
+
       <Footer />
     </div>
   );
